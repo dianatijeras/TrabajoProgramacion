5f29981b33ab1bb8f5a936056265cfa57c327391
dianatijeras <dianam.garciaa1@uqvirtual.edu.co>
2024-02-20 17:55:10 -0500

    creacion clase perro

---
+++ trabajo1/src/main/java/co/edu/uniquindio/poo/App.java
@@ -0,0 +1,17 @@
+package co.edu.uniquindio.poo;
+
+public class App {
+    public static void main (String[] args){
+
+        Perro miPerro = new Perro ("Duna","BorderCollie");
+
+       /// miPerro.caminar(30);
+
+        for (int i = 0; i <=3; i++){
+            miPerro.caminar(30);
+        }
+
+        
+    }
+    
+}
