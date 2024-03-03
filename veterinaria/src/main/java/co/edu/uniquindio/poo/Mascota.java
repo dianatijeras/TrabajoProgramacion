e4a5e2f49d20ace95ccbfc49eb41da4e1348e3a2
dianatijeras <dianam.garciaa1@uqvirtual.edu.co>
2024-03-03 14:52:25 -0500

    cambios en las clases y en los test

--- veterinaria/src/main/java/co/edu/uniquindio/poo/Mascota.java
+++ veterinaria/src/main/java/co/edu/uniquindio/poo/Mascota.java
@@ -10,14 +10,14 @@ public record Mascota(String id, String nombre, String especie, String raza, byt
 
 public Mascota {
 
-    assert id != null && id.isBlank() : "debe llenar este espacio";
-    assert nombre != null && !nombre.isBlank() : "debe llenar este espacio";
-    assert especie != null && !especie.isBlank() : "debe llenar este espacio";
-    assert raza != null && !raza.isBlank() : "debe llenar este espacio";
+    assert id != null : "debe llenar este espacio";
+    assert nombre != null : "debe llenar este espacio";
+    assert especie != null : "debe llenar este espacio";
+    assert raza != null: "debe llenar este espacio";
     assert edad >= 0 :"la edad debe ser mayor a 0";
     //toLowerCase() convierte la cadena genero a minusculas antes de verificar si contiene "femenino" o "masculino". por lo tanto, si el usuario ingresa "Femenino","femenino","Masculino" o "masculino", al convertir estas cadenas a minuscula, todas se veran igual: "femenino" o "masculino"
     assert genero != null && genero.toLowerCase().contains("femenino") || genero.toLowerCase().contains("masculino");
-    assert color != null && !color.isBlank() : "debe llenar este espacio";
+    assert color != null : "debe llenar este espacio";
     assert peso >= 0 : "el peso debe ser mayor o igual a 0";
     }
     
--- veterinaria/src/main/java/co/edu/uniquindio/poo/Veterinaria.java
+++ veterinaria/src/main/java/co/edu/uniquindio/poo/Veterinaria.java
@@ -7,31 +7,42 @@ import java.util.LinkedList;
 public class Veterinaria {
     private final String nombre;
     private final Collection<Mascota> listaMascotas;
-    
+
+    //constructor de la clase Veterinaria
     public Veterinaria(String nombre){
+        //Asegura que el nombre no sea nulo
         assert nombre != null : "debe llenar este espacio";
 
+        //Inicializa el nombre y la lista de mascotas
         this.nombre = nombre;
         this.listaMascotas = new LinkedList<Mascota>();
     }
 
+    //Metodo para obtener el nombre de la Veterinaria
     public String getNombre(){
         return nombre;
     }
     
+    //Metodo para obtener una coleccion no modificable de mascotas, con unmodifiableCollection no se puede modificar el espacio de memorio pero si se pueden añadir objetos
     public Collection<Mascota> getListaMascotas(){
         return Collections.unmodifiableCollection(listaMascotas);
     }
 
+    //metodo para registrar una nueva mascota
     public void registrarMascota (Mascota mascota){
+        //Verifica que el ID de la mascota no exista ya en la lista
         assert verificarId(mascota.id()) == false : "El ID de la mascota ya existe" + mascota.id() + "No se pudo agregar la mascota";
+        //Agrega la mascota a la lista
         listaMascotas.add(mascota);
     }
 
+    //Metodo para verificar si un ID de una mascota ya existe en la lista
     public boolean verificarId(String id){
         boolean existeMascota = false;
 
+        //Itera sobre la lista de mascotas
         for (Mascota mascota : listaMascotas){
+            //Si encuentra una mascota con el mismo ID, establece existeMascota a verdadero
             if (mascota.id().equals(id)){
                 existeMascota = true;
             }
@@ -39,10 +50,14 @@ public class Veterinaria {
         return existeMascota;
     }
 
+    //Metodo para obtener una coleccion de mascotas mayores de 10 años
     public Collection<Mascota> getMascotasMayores10(){
         Collection<Mascota> mascotasMayores10 = new LinkedList<>();
 
+        //Itera sobre la lista mascotas 
         for (Mascota mascota : listaMascotas){
+
+            //Si la edad de la mascota es mayor que 10, la agrega a la lista
             if (mascota.edad() > 10){
                 mascotasMayores10.add(mascota);
             }
